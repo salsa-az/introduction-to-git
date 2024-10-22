762cf8ae79af5a6598ccfe1592a0fa8900f779b6
YusrilHasanuddin <yusrilhasan777@gmail.com>
2024-10-22 16:11:43 +0800

    test pre-commit

--- this_python_file.py
+++ this_python_file.py
@@ -1,13 +1,16 @@
-import os
-import sys
+# import os
+# import sys
+
 
 def greet(name):
     print(f"Hello, {name}!")  # Intentionally correct formatted line
 
-def add(a,b):  # Missing space after comma
-  return a+b  # Wrong indentation (Black should fix this)
 
-greet( "World")  # Extra space inside parentheses (Black should fix this)
+def add(a, b):  # Missing space after comma
+    return a + b  # Wrong indentation (Black should fix this)
+
+
+greet("World")  # Extra space inside parentheses (Black should fix this)
 
 # Trailing whitespace issue below (this should be removed by pre-commit)
-greet("Pre-commit")    
+greet("Pre-commit")
