075a33b26d79f7525593a5358b3b8032af45d884
YusrilHasanuddin <yusrilhasan777@gmail.com>
2024-10-22 16:05:00 +0800

    add pre-commit

--- this_python_file.py
+++ this_python_file.py
@@ -1,1 +1,13 @@
-print("test")
+import os
+import sys
+
+def greet(name):
+    print(f"Hello, {name}!")  # Intentionally correct formatted line
+
+def add(a,b):  # Missing space after comma
+  return a+b  # Wrong indentation (Black should fix this)
+
+greet( "World")  # Extra space inside parentheses (Black should fix this)
+
+# Trailing whitespace issue below (this should be removed by pre-commit)
+greet("Pre-commit")    
